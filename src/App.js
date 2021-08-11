cfae4d230fa67ea599e33890ba9d9c4bdf38e6f2
Justyna Rachowicz <mrowa44@gmail.com>
2021-08-11 15:59:36 +0200

    Add basic page layout

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './index.css';
-import './App.css';
+import PageLayout from './PageLayout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageLayout
+      headerText="Event form"
+      onPageAction={() => {}}
+      pageActionLabel="Edit"
+    >
+      text
+    </PageLayout>
   );
 }
 
---
+++ src/components/buttons/SecondaryBtn.js
@@ -0,0 +1,35 @@
+/* eslint-disable react/button-has-type */
+/* https://github.com/yannickcr/eslint-plugin-react/issues/1846#issuecomment-614921516 */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function SecondaryBtn(props) {
+  const {
+    children,
+    type = 'button',
+    onClick,
+  } = props;
+  return (
+    <>
+      <button
+        type={type}
+        onClick={onClick}
+        className="border rounded-md px-8 hover:bg-gray-50"
+      >
+        {children}
+      </button>
+    </>
+  );
+}
+
+SecondaryBtn.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired,
+  type: PropTypes.oneOf(['button', 'submit']),
+};
+
+SecondaryBtn.defaultProps = {
+  type: 'button',
+};
+
+export default SecondaryBtn;
