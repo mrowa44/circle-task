efa4b7e34411f3fb4bfcd1a3200e7024c1393ed0
Justyna Rachowicz <4957913+mrowa44@users.noreply.github.com>
2021-08-13 18:42:51 +0200

    Setup routing (#4)
    * Add prop-types
    
    * Add basic page layout
    
    * Cleanup
    
    * Add routing
    
    * Add forgotten isRequired

---
+++ src/pages/EditEventPage.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import PageLayout from '../PageLayout';
+
+function EditEventPage(props) {
+  const { match, history } = props;
+  const { params } = match;
+  const { id } = params;
+  const handleAction = () => {
+    history.push('/create');
+  };
+  return (
+    <PageLayout
+      headerText="Event form"
+      onPageAction={handleAction}
+      pageActionLabel="Cancel"
+    >
+      edit page - id:&nbsp;
+      {id}
+    </PageLayout>
+  );
+}
+
+EditEventPage.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default EditEventPage;
