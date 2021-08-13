79f5e21a430a79abb3b4cf3111de5d7cb8539a63
Justyna Rachowicz <mrowa44@gmail.com>
2021-08-13 17:55:50 +0200

    Add edit form page

--- src/pages/EditEventPage.js
+++ src/pages/EditEventPage.js
@@ -2,22 +2,50 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import PageLayout from '../PageLayout';
+import EventForm from '../components/EventForm';
+
+function getEventData(eventId) {
+  // Mocked event
+  return {
+    id: eventId,
+    name: `Test Event ${eventId}`,
+    place: 'in-person',
+    datetime: new Date(),
+    duration: '2h',
+    description: 'Test Event description',
+    slug: 'test',
+  };
+}
 
 function EditEventPage(props) {
   const { match, history } = props;
   const { params } = match;
   const { id } = params;
+
   const handleAction = () => {
     history.push('/create');
   };
+  const handleSubmit = (data) => {
+    // Send data to API
+    console.log(data);
+  };
+  const [apiEvent, setApiEvent] = React.useState(null);
+  React.useEffect(() => {
+    const event = getEventData(id);
+    setApiEvent(event);
+  }, [id]);
+
   return (
     <PageLayout
       headerText="Event form"
       onPageAction={handleAction}
       pageActionLabel="Cancel"
     >
-      edit page - id:&nbsp;
-      {id}
+      <EventForm
+        onSubmit={handleSubmit}
+        submitLabel="Save"
+        initialValues={apiEvent}
+      />
     </PageLayout>
   );
 }
