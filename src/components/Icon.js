f6883691c928d7bfb74d880c34d96b0d27a18406
Justyna Rachowicz <mrowa44@gmail.com>
2021-08-14 14:03:00 +0200

    Create Icon component

---
+++ src/components/Icon.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+
+import cross from '../assets/cross.svg';
+import open from '../assets/open_icon.svg';
+import privateIcon from '../assets/private icon.svg';
+
+const ICONS = {
+  cross,
+  open,
+  private: privateIcon,
+};
+
+function Icon(props) {
+  const { type, alt } = props;
+  const src = ICONS[type];
+
+  if (!src) {
+    return null;
+  }
+
+  return <img src={ICONS[type]} alt={alt} />;
+}
+
+Icon.propTypes = {
+  alt: PropTypes.string,
+  type: PropTypes.string.isRequired,
+};
+
+Icon.defaultProps = {
+  alt: 'icon',
+};
+
+export default Icon;
